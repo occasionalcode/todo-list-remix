b1d83f3cd764431ff8c67a0d88ba99d5926a4f7f
GABRIEL IBANEZ <occasionalprogrammer@gmail.com>
2024-05-03 21:55:31 +0800

    build: tailwind CSS configuration

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { LinksFunction } from "@remix-run/server-runtime";
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
